59987712057ef76ddf96c7558be39788fb94b8a8
merce <merce@192.168.1.156>
2023-01-15 22:04:53 +0100

    Versión cero del proyecto.
    Signed-off-by: muriosh
    Signed-off-by: merce <merce@192.168.1.156>

---
+++ src/pintaemojis/PintaEmojis.java
@@ -0,0 +1,13 @@
+package pintaemojis;
+
+public class PintaEmojis {
+
+	public static void main(String[] args) {
+		for (int i=0; i<10; i++) {
+			System.out.printf("\uD83D\uDE00");
+			System.out.println("\uD83D\uDE02");
+		}
+		System.out.println("\uD83D\uDE02");
+	}
+
+}
