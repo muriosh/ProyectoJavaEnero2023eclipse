24565b3779da84b5e538a1579d9e70e13043fd96
muriosh <75389767+muriosh@users.noreply.github.com>
2023-01-23 21:00:01 +0100

    Update PintaEmojis.java
    Cambio estético, con una línea separadora

--- src/pintaemojis/PintaEmojis.java
+++ src/pintaemojis/PintaEmojis.java
@@ -6,6 +6,7 @@ public class PintaEmojis {
 		for (int i=0; i<15; i++) {
 			System.out.printf("\uD83D\uDE00");
 			System.out.println("\uD83D\uDE02");
+			System.out.println("------------");
 		}
 		System.out.println("\uD83D\uDE02");
 	}
