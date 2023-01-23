6712ee01cecff20bf5f4aa6d2344c71fa9308d67
muriosh <75389767+muriosh@users.noreply.github.com>
2023-01-23 20:20:31 +0100

    Update PintaEmojis.java
    Se han añadido mas emojis y se pintan en un bucle de 15 iteraciones.

--- src/pintaemojis/PintaEmojis.java
+++ src/pintaemojis/PintaEmojis.java
@@ -3,7 +3,7 @@ package pintaemojis;
 public class PintaEmojis {
 
 	public static void main(String[] args) {
-		for (int i=0; i<10; i++) {
+		for (int i=0; i<15; i++) {
 			System.out.printf("\uD83D\uDE00");
 			System.out.println("\uD83D\uDE02");
 		}
